b00a5ea5baaa7415971eef1644b08209600b4b7e
Marina Rumyantsev <aniramrum@gmail.com>
2018-11-25 23:28:26 +0200

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyByNyvx-t_2-cdlfe0XTgx9qxmeJDXx_WM',
+    authDomain: 'oshop-37ca5.firebaseapp.com',
+    databaseURL: 'https://oshop-37ca5.firebaseio.com',
+    projectId: 'oshop-37ca5',
+    storageBucket: 'oshop-37ca5.appspot.com',
+    messagingSenderId: '771035667756'
+  }
 };
