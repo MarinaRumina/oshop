7e59313e1c678f4c6c26ad61a058c9c8eb049ba7
Marina Rumyantsev <aniramrum@gmail.com>
2018-12-04 01:23:04 +0200

    auth update

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import 'rxjs/add/operator/map';
+
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,13 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate() {
-    return this.auth.user$.map( user => {
-      if (user) {
-        return true;
-      }
-
-      this.router.navigate(['/login']);
-      return false;
-    });
+    return this.auth.user$.pipe(map( user => {
+        if (user) {
+          return true;
+        }
+        this.router.navigate(['/login']);
+        return false;
+      })
+    );
   }
 }
